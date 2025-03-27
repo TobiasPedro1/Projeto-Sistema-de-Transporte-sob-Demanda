a7b4d1ec01e43d2665e3e561858fcade521a4a4d
pedro tobias <pedrotsg123@gmail.com>
2025-03-27 15:19:11 -0300

    atualizar com tratamento de erro

--- src/model/Pagamento.java
+++ src/model/Pagamento.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Pagamento {
+    public static void pagarCorrida(Cliente cliente, Motorista motorista, double valor){
+        if(cliente.getConta().getSaldo() >= valor){
+            cliente.getConta().sacar(valor);
+            motorista.getConta().depositar(valor);
+            System.out.println("Pagamento efetuado com sucesso!");
+        } else {
+            //lancar excecao
+            System.out.println("Saldo insuficiente para efetuar o pagamento");
+        }
+    }
 }
