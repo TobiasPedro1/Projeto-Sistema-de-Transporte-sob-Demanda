592ada6f801b7037ad8c7c3a0c80905434f6ad98
pedro tobias <pedrotsg123@gmail.com>
2025-03-27 18:45:26 -0300

    .

--- src/model/Viagem.java
+++ src/model/Viagem.java
@@ -1,4 +1,37 @@
 package model;
 
 public class Viagem {
+    private String origem;
+    private String destino;
+    private double valor;
+
+    public Viagem(String origem, String destino, double valor) {
+        this.origem = origem;
+        this.destino = destino;
+        this.valor = valor;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
