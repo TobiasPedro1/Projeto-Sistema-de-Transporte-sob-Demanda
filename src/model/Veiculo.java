51128a25b710d6df55324f8854ec135315d7b027
pedro tobias <pedrotsg123@gmail.com>
2025-03-27 15:53:17 -0300

    revisar

--- src/model/Veiculo.java
+++ src/model/Veiculo.java
@@ -1,4 +1,59 @@
 package model;
 
+import model.enums.TipoVeiculo;
+
 public class Veiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private TipoVeiculo tipoVeiculo;
+
+    public Veiculo(String placa, String marca, String modelo, int ano, TipoVeiculo tipoVeiculo) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.tipoVeiculo = tipoVeiculo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public TipoVeiculo getTipoVeiculo() {
+        return tipoVeiculo;
+    }
+
+    public void setTipoVeiculo(TipoVeiculo tipoVeiculo) {
+        this.tipoVeiculo = tipoVeiculo;
+    }
 }
