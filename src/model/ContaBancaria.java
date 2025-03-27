be19ca935dbaa63cdf0539fdd7a3c1b51be19f6b
pedro tobias <pedrotsg123@gmail.com>
2025-03-27 15:19:33 -0300

    atualizar com tratamento de erro

---
+++ src/model/ContaBancaria.java
@@ -0,0 +1,47 @@
+package model;
+
+public class ContaBancaria {
+    private Integer numeroConta;
+    private Double saldo;
+
+    public ContaBancaria(Integer numeroConta){
+        this.numeroConta = numeroConta;
+        this.saldo = 0.0;
+    }
+
+    public Integer getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(Integer numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(Double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+        } else {
+            //lancar excecao
+            System.out.println("Valor de depósito inválido");
+        }
+    }
+
+    public void sacar(Double valor) {
+        if (valor > 0 && this.saldo >= valor) {
+            this.saldo -= valor;
+        } else {
+            //lancar excecao
+            System.out.println("Valor de saque inválido ou saldo insuficiente");
+        }
+    }
+
+
+}
