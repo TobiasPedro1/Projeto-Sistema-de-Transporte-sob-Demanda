fd2e3f1a3fa79fb50f0912f5f5c2fb8dde1aa6e2
pedro tobias <pedrotsg123@gmail.com>
2025-03-28 11:34:28 -0300

    .

--- src/model/ViagemEntrega.java
+++ src/model/ViagemEntrega.java
@@ -1,4 +1,23 @@
 package model;
 
-public class ViagemEntrega {
+import model.enums.StatusViagem;
+import service.MotoristaService;
+
+public class ViagemEntrega extends Viagem {
+    private String encomenda;
+
+
+    public ViagemEntrega(String origem, String destino, double valor, Veiculo veiculo, String encomenda, StatusViagem statusViagem, MotoristaService motoristaService) {
+        super(origem, destino, valor, veiculo, statusViagem, motoristaService );
+        this.encomenda = encomenda;
+    }
+
+    public String getEncomenda() {
+        return encomenda;
+    }
+
+    public void setEncomenda(String encomenda) {
+        this.encomenda = encomenda;
+    }
+
 }
