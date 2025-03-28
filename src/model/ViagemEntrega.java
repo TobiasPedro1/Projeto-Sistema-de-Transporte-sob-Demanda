34204b3c993678c2307135766ff8dac2b3b165c0
pedro tobias <pedrotsg123@gmail.com>
2025-03-28 14:52:13 -0300

    verificar depois

--- src/model/ViagemEntrega.java
+++ src/model/ViagemEntrega.java
@@ -6,7 +6,6 @@ import service.MotoristaService;
 public class ViagemEntrega extends Viagem {
     private String encomenda;
 
-
     public ViagemEntrega(String origem, String destino, double valor, Veiculo veiculo, String encomenda, StatusViagem statusViagem, MotoristaService motoristaService) {
         super(origem, destino, valor, veiculo, statusViagem, motoristaService );
         this.encomenda = encomenda;
