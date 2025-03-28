7617e6f410b7fabf2e718778286278b4223c17a3
pedro tobias <pedrotsg123@gmail.com>
2025-03-28 10:38:17 -0300

    possivel alteração apos a criação do repositorio

--- src/model/ViagemPassageiro.java
+++ src/model/ViagemPassageiro.java
@@ -1,4 +1,31 @@
 package model;
 
-public class ViagemPassageiro {
+import service.MotoristaService;
+
+public class ViagemPassageiro extends Viagem {
+    private String passageiro;
+    private Motorista motorista;
+
+    public ViagemPassageiro(String origem, String destino, double valor, Veiculo veiculo, String passageiro, MotoristaService motoristaService) {
+        super(origem, destino, valor, veiculo);
+        this.passageiro = passageiro;
+        //possivel alteração apos a criação do repositorio
+        this.motorista = motoristaService.selecionarMotoristaAleatorio();
+    }
+
+    public String getPassageiro() {
+        return passageiro;
+    }
+
+    public void setPassageiro(String passageiro) {
+        this.passageiro = passageiro;
+    }
+
+    public Motorista getMotorista() {
+        return motorista;
+    }
+
+    public void setMotorista(Motorista motorista) {
+        this.motorista = motorista;
+    }
 }
