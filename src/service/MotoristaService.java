e1e674f82eaa3618cfdf3a7ab1d8c0c681c8130a
pedro tobias <pedrotsg123@gmail.com>
2025-03-28 11:35:57 -0300

    aleterar para repositorio assim que criado

--- src/service/MotoristaService.java
+++ src/service/MotoristaService.java
@@ -3,9 +3,14 @@ package service;
 import model.Avaliacao;
 import model.Motorista;
 import static utils.VerificaCpf.verificaCPF;
-import utils.VerificaCpf;
+import java.util.List;
+import java.util.Random;
 
 public class MotoristaService {
+    //aleterar para repositorio assim que criado
+    private List<Motorista> motoristasList;
+
+
     public boolean validarMotorista(Motorista motorista){
 
         if(verificaCPF(motorista.getCpf()) && motorista.getHabilitacao().length() == 11){
@@ -20,4 +25,16 @@ public class MotoristaService {
     public void adicionarAvaliacao(Motorista motorista ,Avaliacao avaliacao){
         motorista.adicionarAvaliacao(avaliacao);
     }
+
+
+    //aleterar para repositorio assim que criado
+    public Motorista selecionarMotoristaAleatorio(){
+        Random random = new Random();
+        int index = random.nextInt(this.motoristasList.size());
+
+        return this.motoristasList.get(index);
+
+    }
+
+
 }
