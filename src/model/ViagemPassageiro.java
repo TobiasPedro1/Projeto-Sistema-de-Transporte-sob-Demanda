a1371b8d65e0ac93b9c727b67541778e08ef21cd
pedro tobias <pedrotsg123@gmail.com>
2025-03-29 18:57:48 -0300

    .

--- src/model/ViagemPassageiro.java
+++ src/model/ViagemPassageiro.java
@@ -1,13 +1,12 @@
 package model;
 
-import model.enums.StatusViagem;
 import service.MotoristaService;
 
 public class ViagemPassageiro extends Viagem {
     private String passageiro;
 
-    public ViagemPassageiro(String origem, String destino, double valor, Veiculo veiculo, String passageiro, StatusViagem statusViagem, MotoristaService motoristaService) {
-        super(origem, destino, valor, veiculo, statusViagem,motoristaService);
+    public ViagemPassageiro(String origem, String destino, double valor, Veiculo veiculo, String passageiro, Motorista motorista) {
+        super(origem, destino, valor, veiculo,motorista);
         this.passageiro = passageiro;
     }
 
