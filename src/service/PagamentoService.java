7778791b66a6bfdfa022395012dbb18b88a9cb09
pedro tobias <pedrotsg123@gmail.com>
2025-03-29 20:46:34 -0300

    .

--- src/service/PagamentoService.java
+++ src/service/PagamentoService.java
@@ -3,16 +3,15 @@ package service;
 import model.Cliente;
 import model.Motorista;
 import model.Pagamento;
-import model.enums.FormaDePagamento;
 
 public class PagamentoService {
-    public Pagamento pagarCorrida(Cliente cliente, Motorista motorista, double valor, FormaDePagamento formaDePagamento){
+    public Pagamento pagarCorrida(Cliente cliente, Motorista motorista, double valor){
         if(cliente.getConta().getSaldo() >= valor){
             cliente.getConta().sacar(valor);
             motorista.getConta().depositar(valor);
             System.out.println("Pagamento efetuado com sucesso!");
 
-            return new Pagamento(cliente, motorista, valor, formaDePagamento );
+            return new Pagamento(cliente, motorista, valor );
         } else {
             //lancar excecao especifica
             throw new RuntimeException("Saldo insuficiente para efetuar o pagamento");
