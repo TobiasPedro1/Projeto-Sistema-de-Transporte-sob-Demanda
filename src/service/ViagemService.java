e3d0875eaefb890dcdaac3550d3c9638d07d7215
pedro tobias <pedrotsg123@gmail.com>
2025-03-29 18:51:18 -0300

    .

--- src/service/ViagemService.java
+++ src/service/ViagemService.java
@@ -7,12 +7,11 @@ import repository.ClienteRepository;
 import repository.MotoristaRepository;
 import repository.ViagemRepository;
 
-public class ViagemService {
+public class ViagemService implements ViagemServiceInterface {
     private ClienteRepository clienteRepository;
     private MotoristaRepository motoristaRepository;
     private MotoristaService motoristaService;
     private ViagemRepository viagemRepository;
-
 
     public ViagemService(ClienteRepository clienteRepository, MotoristaRepository motoristaRepository, MotoristaService motoristaService, ViagemRepository viagemRepository) {
         this.clienteRepository = clienteRepository;
@@ -21,50 +20,68 @@ public class ViagemService {
         this.viagemRepository = viagemRepository;
     }
 
-    // Implementação do serviço de viagem
-
+    @Override
     public Viagem chamarViagem(String origem, String destino, double valor, Cliente cliente) {
         Motorista motorista = motoristaService.selecionarMotoristaAleatorio();
 
-        if(motorista == null || !motorista.isDisponivel()){
-            //lancar excecao
+        if (motorista == null || !motorista.isDisponivel()) {
             System.out.println("Nenhum motorista disponível no momento.");
             return null;
         } else if (!cliente.isValidado()) {
-            //lancar excecao
             System.out.println("Cliente não validado.");
             return null;
         }
 
         Viagem viagem = new Viagem(origem, destino, valor, motorista.getVeiculo(), motorista);
+        motorista.setDisponivel(false);
 
         clienteRepository.save(cliente);
         motoristaRepository.save(motorista);
-
-
         viagemRepository.save(viagem);
 
-        System.out.println("Viagem chamada por: " + cliente.getNome() + ", de origem: "+origem + "com destino: "+destino + " com motorista: " + motorista.getNome());
+        System.out.println("Viagem chamada por: " + cliente.getNome() + ", de origem: " + origem + " com destino: " + destino + " com motorista: " + motorista.getNome());
         return viagem;
     }
 
+    @Override
+    public Viagem chamarViagemEntrega(String origem, String destino, double valor, String encomenda) {
+        Motorista motorista = motoristaService.selecionarMotoristaAleatorio();
+
+        if (motorista == null || !motorista.isDisponivel()) {
+            System.out.println("Nenhum motorista disponível no momento.");
+            return null;
+        }
+
+        Viagem viagem = new Viagem(origem, destino, valor, motorista.getVeiculo(), motorista);
+        motorista.setDisponivel(false);
+
+        motoristaRepository.save(motorista);
+        viagemRepository.save(viagem);
+
+        System.out.println("Viagem de entrega chamada de origem: " + origem + " com destino: " + destino + " com motorista: " + motorista.getNome() + " do pacote: " + encomenda);
+        return viagem;
+    }
+
+    @Override
     public void iniciarViagem() {
         System.out.println("Viagem iniciada.");
     }
 
+    @Override
     public void finalizarViagem(Viagem viagem) {
-        System.out.println("Viagem finalizada.");
         Motorista motorista = viagem.getMotorista();
         motorista.setDisponivel(true);
         motoristaRepository.save(motorista);
         viagemRepository.save(viagem);
+        System.out.println("Viagem finalizada.");
     }
 
+    @Override
     public void cancelarViagem(Viagem viagem) {
-        System.out.println("Viagem cancelada.");
         Motorista motorista = viagem.getMotorista();
         motorista.setDisponivel(true);
         motoristaRepository.save(motorista);
         viagemRepository.save(viagem);
+        System.out.println("Viagem cancelada.");
     }
 }
