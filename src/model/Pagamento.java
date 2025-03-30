69b127d64a08e33706945781218661fa3dd11c6c
pedro tobias <pedrotsg123@gmail.com>
2025-03-30 14:34:53 -0300

    .

--- src/model/Pagamento.java
+++ src/model/Pagamento.java
@@ -1,22 +1,19 @@
 package model;
 
-import model.enums.FormaDePagamento;
 import java.time.LocalDateTime;
 
 public class Pagamento {
     private Cliente cliente;
     private Motorista motorista;
     private double valor;
-    private FormaDePagamento formaDePagamento;
     private LocalDateTime dataHoraPagamento;
 
     public Pagamento(){}
 
-    public Pagamento(Cliente cliente, Motorista motorista, double valor,FormaDePagamento formaDePagamento){
+    public Pagamento(Cliente cliente, Motorista motorista, double valor ){
         this.cliente = cliente;
         this.motorista = motorista;
         this.valor = valor;
-        this.formaDePagamento = formaDePagamento;
         this.dataHoraPagamento = LocalDateTime.now();
     }
 
@@ -30,10 +27,6 @@ public class Pagamento {
 
     public double getValor() {
         return valor;
-    }
-
-    public FormaDePagamento getFormaDePagamento() {
-        return formaDePagamento;
     }
 
     public LocalDateTime getDataHoraPagamento() {
