acbe687b968ab0b7ca17b21822017c522812037f
pedro tobias <pedrotsg123@gmail.com>
2025-03-30 16:12:32 -0300

    .

--- src/model/VeiculoSuv.java
+++ src/model/VeiculoSuv.java
@@ -1,7 +1,6 @@
 package model;
 
 public class VeiculoSuv extends Veiculo{
-
     public VeiculoSuv(String placa, String marca, String modelo, int qtdDePassageiros, int ano, double fatorMultiplicador) {
         super(placa, marca, modelo, qtdDePassageiros, ano);
     }
