9b046a564282110849e77022e7a904cd9be4349c
pedro tobias <pedrotsg123@gmail.com>
2025-03-30 18:27:54 -0300

    .

---
+++ src/repository/AvaliacaoRepository.java
@@ -0,0 +1,36 @@
+package repository;
+
+import model.Avaliacao;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvaliacaoRepository implements AvaliacaoRepositoryInterface {
+    List<Avaliacao> avalicaoRepository = new ArrayList<>();
+
+    @Override
+    public void save(Avaliacao avaliacao) {
+        avalicaoRepository.add(avaliacao);
+    }
+    @Override
+    public Avaliacao findByComentario(String comentario) {
+        for (Avaliacao avaliacao : avalicaoRepository) {
+            if (avaliacao.getComentario().equals(comentario)) {
+                return avaliacao;
+            }
+        }
+        return null;
+    }
+    @Override
+    public List<Avaliacao> findAll() {
+        return avalicaoRepository;
+    }
+    @Override
+    public void delete(String comentario) {
+        Avaliacao avaliacao = findByComentario(comentario);
+        if (avaliacao != null) {
+            avalicaoRepository.remove(avaliacao);
+        } else{
+            System.out.println("Avaliação não encontrada.");
+        }
+    }
+}
---
+++ src/repository/AvaliacaoRepositoryInterface.java
@@ -0,0 +1,12 @@
+package repository;
+
+import model.Avaliacao;
+
+import java.util.List;
+
+public interface AvaliacaoRepositoryInterface {
+    void save(Avaliacao avaliacao);
+    Avaliacao findByComentario(String comentario);
+    List<Avaliacao> findAll();
+    void delete(String comentario);
+}
