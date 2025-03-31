c87fff228c03055786aada56e70cd129aacf9d7c
pedro tobias <pedrotsg123@gmail.com>
2025-03-31 16:34:00 -0300

    .

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -3,8 +3,15 @@ package service;
 import static utils.VerificaCpf.verificaCPF;
 import model.Cliente;
 import model.Avaliacao;
+import repository.ClienteRepository;
 
 public class ClienteService {
+    private ClienteRepository clienteRepository;
+
+    public ClienteService (ClienteRepository clienteRepository){
+        this.clienteRepository = clienteRepository;
+    }
+
     public boolean validarCliente(Cliente cliente){
         if(verificaCPF(cliente.getCpf())){
             cliente.setValidado(true);
